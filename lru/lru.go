c430fd0632c4d746131af4147175655585dff50e
Karthik Velayutham <vkarthik@ponder.io>
2022-07-04 00:55:12 -0500

    Refined logic, added some functionality

--- lru/lru.go
+++ lru/lru.go
@@ -1,6 +1,7 @@
 package lrucache
 
 import (
+	"container/list"
 	"errors"
 )
 
@@ -8,37 +9,62 @@ import (
 // for its internal representation.
 
 type Entry struct {
-	key   int
+	key   interface{}
 	value interface{}
 }
 
+// This is not thread-safe
 type LRUCache struct {
-	size int
-	head *Entry
+	size       int
+	entryStore map[interface{}]*list.Element // Keep track of entries
+	entryOrder *list.List                    // Keep track of ordering
 }
 
+// Factory function to create new LRU cache
 func NewLRUCache(size int) (*LRUCache, error) {
 	if size <= 0 {
-		return nil, errors.New("need a size greater than 0!")
+		return nil, errors.New("need a size greater than 0")
 	}
 	lru := &LRUCache{
-		size: size,
-		head: &Entry{},
+		size:       size,
+		entryStore: make(map[interface{}]*list.Element),
+		entryOrder: list.New(),
 	}
 	return lru, nil
 }
 
 // Add(key, value) - add entry to the LRU cache
-func (c *LRUCache) Add(key int, value int) error {
-	return nil
+func (c *LRUCache) Add(key interface{}, value interface{}) {
+	// Check if the value already exists in here
+	if val, ok := c.entryStore[key]; ok {
+		// Update the ordering
+		c.entryOrder.MoveToFront(val)
+	}
+	// Store in the entry and store in the ordering
+	elem := c.entryOrder.PushFront(&Entry{key, value})
+	c.entryStore[key] = elem
+	if c.entryOrder.Len() > c.size {
+		c.evictOldest()
+	}
 }
 
 // Get(key) - get entry from the cache
-func (c *LRUCache) Get(key int) error {
-	return nil
+func (c *LRUCache) Get(key interface{}) (interface{}, error) {
+	if _, ok := c.entryStore[key]; !ok {
+		return 0, errors.New("couldn't find key in cache")
+	}
+	val := c.entryStore[key]
+	c.entryOrder.MoveToFront(val)
+	return val.Value.(*Entry).value, nil
 }
 
 // Remove(key) - remove entry from the cache
-func (c *LRUCache) Remove(key int) error {
+func (c *LRUCache) Remove(key interface{}) error {
 	return nil
+}
+
+// evictOldest() - removes the oldest entry from the cache
+func (c *LRUCache) evictOldest() {
+	entry := c.entryOrder.Remove(c.entryOrder.Back())
+	delete(c.entryStore, entry.(Entry).key)
 }
--- main.go
+++ main.go
@@ -10,6 +10,13 @@ func main() {
 	if err != nil {
 		fmt.Println("Looks like we have a problem with NewLRUCache!")
 	}
+
+	lru.Add("lalala", 42)
+	val, err := lru.Get("lalala")
+	if err != nil {
+		fmt.Println("Looks like the value didn't exist?")
+	}
+	fmt.Println(val)
 	fmt.Println("blah blah")
 
 }
